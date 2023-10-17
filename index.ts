660a8c6fd7fbb2df45b99bb7c00c01f47051c08d
acomarcho <acoxstpd@gmail.com>
2023-10-17 18:58:17 +0700

    Feat: Add helmet, morgan, and rate limiter

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@ import "module-alias/register";
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 
 import CategoryRouter from "@routes/category";
 
@@ -9,6 +12,9 @@ dotenv.config();
 
 const app: Express = express();
 app.use(cors());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(rateLimit({ windowMs: 1000 * 60, limit: 100 }));
 
 app.use("/category", CategoryRouter);
 
