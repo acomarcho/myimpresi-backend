b43f373f6c3ea0a53da8edf80ee039304c5bafd2
acomarcho <acoxstpd@gmail.com>
2023-10-18 08:42:12 +0700

    Feat: Add SaveProduct model

---
+++ models/product.ts
@@ -0,0 +1,32 @@
+import prisma from "@utils/prisma";
+import { Product } from "@prisma/client";
+
+const SaveProduct = async (product: Product, imagePaths: string[]) => {
+  const newProduct = await prisma.$transaction(async (tx) => {
+    const createdProduct = await tx.product.create({
+      data: {
+        name: product.name,
+        price: product.price,
+        soldAmount: product.soldAmount,
+        minimumOrder: product.minimumOrder,
+        description: product.description,
+        subcategoryId: product.subcategoryId,
+      },
+    });
+
+    for (const imagePath of imagePaths) {
+      await tx.productImage.create({
+        data: {
+          path: imagePath,
+          productId: createdProduct.id,
+        },
+      });
+    }
+  });
+
+  return newProduct;
+};
+
+export default {
+  SaveProduct,
+};
