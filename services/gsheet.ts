217975d646f103a3103210bc7e7daf5828574360
acomarcho <acoxstpd@gmail.com>
2023-10-19 13:27:26 +0700

    Fix: Use query params instead of JSON body

--- services/gsheet.ts
+++ services/gsheet.ts
@@ -7,18 +7,12 @@ import { RecaptchaResponse } from "@constants/responses";
 
 const AppendContact = async (req: AppendContactRequest) => {
   const response = await axios.put(
-    "https://www.google.com/recaptcha/api/siteverify",
-    {
-      secret: process.env.RECAPTCHA_SECRET_KEY || "",
-      response: req.recaptchaToken,
-    }
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${req.recaptchaToken}`
   );
   const recaptchaResponse: RecaptchaResponse = response.data;
 
-  console.log(recaptchaResponse);
-
   if (!recaptchaResponse.success) {
-    throw createHttpError(401, null, "Invalid reCAPTCHA token");
+    throw createHttpError(401, recaptchaResponse, "Invalid reCAPTCHA token");
   }
 
   const spreadsheetId = process.env.SPREADSHEET_ID;
