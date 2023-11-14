6bdeb46c369350e2d28831e3897a3a1d362f228a
acomarcho <acoxstpd@gmail.com>
2023-11-14 16:39:35 +0700

    Feat: Add SaveArticle route

--- routes/article.ts
+++ routes/article.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import ArticleController from "@controllers/article";
 
+import { upload } from "../middlewares/multer";
+import { checkApiKey } from "../middlewares/api-key";
+
 const router = express.Router();
 
 // GET /article
 router.get("/", ArticleController.FindAllArticles);
+// POST /article
+router.post(
+  "/",
+  checkApiKey,
+  upload.single("file"),
+  ArticleController.SaveArticle
+);
 
 export default router;
