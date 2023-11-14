2345bdd5977866b40d3a8fd35bb98e7d07d8e8da
acomarcho <acoxstpd@gmail.com>
2023-11-14 16:38:46 +0700

    Feat: Add SaveArticle controller

--- controllers/article.ts
+++ controllers/article.ts
@@ -1,6 +1,7 @@
 import ArticleService from "@services/article";
 import { Request, Response } from "express";
 import { createHttpError, handleError } from "@utils/error";
+import { SaveArticleRequest } from "@constants/requests";
 
 const FindAllArticles = async (req: Request, res: Response) => {
   try {
@@ -11,6 +12,39 @@ const FindAllArticles = async (req: Request, res: Response) => {
   }
 };
 
+const SaveArticle = async (req: Request, res: Response) => {
+  try {
+    const file = req.file;
+
+    if (!file) {
+      throw createHttpError(400, null, "No file supplied");
+    }
+
+    const { title, content }: SaveArticleRequest = req.body;
+
+    if (!title) {
+      throw createHttpError(400, null, "No title supplied");
+    }
+
+    if (!content) {
+      throw createHttpError(400, null, "No content supplied");
+    }
+
+    const newArticle = await ArticleService.SaveArticle({
+      title,
+      content,
+      file,
+    });
+
+    res.status(200).json({
+      data: newArticle,
+    });
+  } catch (e) {
+    handleError(e, res);
+  }
+};
+
 export default {
   FindAllArticles,
+  SaveArticle,
 };
