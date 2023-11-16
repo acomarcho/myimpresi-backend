3fbe4af9f49dbe34572aa0ab36e16c307335007c
acomarcho <acoxstpd@gmail.com>
2023-11-16 21:30:52 +0700

    Feat: Add save event route

--- routes/event.ts
+++ routes/event.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import EventController from "@controllers/event";
+import bodyParser from "body-parser";
 
 const router = express.Router();
 
 // GET /event
 router.get("/", EventController.FindAllEvents);
+// POST /event
+router.post("/", bodyParser.json(), EventController.SaveEvent);
 
 export default router;
