4f39eb7dd9e0bc21542c3cf8a3a6f0d67ffc568e
acomarcho <acoxstpd@gmail.com>
2023-11-16 21:39:43 +0700

    Feat: Add checkApiKey middleware to POST /event

--- routes/event.ts
+++ routes/event.ts
@@ -2,11 +2,13 @@ import express from "express";
 import EventController from "@controllers/event";
 import bodyParser from "body-parser";
 
+import { checkApiKey } from "../middlewares/api-key";
+
 const router = express.Router();
 
 // GET /event
 router.get("/", EventController.FindAllEvents);
 // POST /event
-router.post("/", bodyParser.json(), EventController.SaveEvent);
+router.post("/", checkApiKey, bodyParser.json(), EventController.SaveEvent);
 
 export default router;
